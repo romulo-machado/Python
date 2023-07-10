a230e9e1dc89680df8628c9a21b1a443c70cd826
romulo machado <romulomachado1@outlook.com>
2023-07-10 13:08:40 -0300

    commit inicial

---
+++ mul2.py
@@ -0,0 +1,4 @@
+# Calculadora de multiplos de 2
+num = int(input("Digite um valor a ser multiplicado por 2 \n"))
+num = num * 2
+print(num)
---
+++ par-impa.py
@@ -0,0 +1,7 @@
+print('Par ou Impar')
+num = float(input('Qual o número que vc deseja saber? \n'))
+resultado = num % 2
+if resultado == 0:
+    print('O número digitado é par.')
+else:
+    print('O número digitado é impa')
