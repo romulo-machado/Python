21ad506c7ac62cb4178ce51ca32940f180455687
romulo machado <romulomachado1@outlook.com>
2023-07-26 10:38:30 -0300

    criado sistema de login.

--- sistema-de-login.py
+++ sistema-de-login.py
@@ -1,5 +1,7 @@
 nome = input('Digite seu nome: ')
+sobre_nome = input('Digite seu sobre nome: ')
 idade = int(input('Digite sua idade: '))
 
 print(f'{nome}')
-print(f'{idade}')
+print(f'{sobre_nome}')
+print(f'{idade} anos')
